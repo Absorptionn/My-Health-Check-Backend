33fadedbc7ea2d050dd666235565b612c2dc4750
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2022-04-06 12:51:36 +0800

    Updated CORS

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ const not_found = require("./middleware/not_found");
 const error_handler = require("./middleware/error_handler");
 
 const app = express();
-app.use(cors());
+
+const whitelist = [
+	"https://auf-myhealthcheck-dashboard.netlify.app",
+	"https://auf-myhealthcheck-survey.netlify.app",
+];
+
+var cors_options_delegate = function (req, callback) {
+	var corsOptions;
+	if (whitelist.indexOf(req.header("Origin")) !== -1) {
+		corsOptions = { origin: true };
+	} else {
+		corsOptions = { origin: false };
+	}
+	callback(null, corsOptions);
+};
+
+app.use(cors(cors_options_delegate));
 app.use(express.json());
 app.use("/api/v1/hmwa", router);
 app.use(not_found);
