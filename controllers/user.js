5ea9433c4beaf4173f2586a5c8cd488133150346
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2022-04-22 14:48:33 +0800

    Changed User Creation

--- controllers/user.js
+++ controllers/user.js
@@ -5,21 +5,7 @@ const bcrypt = require("bcrypt");
 const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
 
-const create_user = async_wrapper(async (req, res) => {
-	const { username, password } = req.body.user;
-	const hashed_password = await bcrypt.hash(password, 12);
-	const user = { username, password: hashed_password };
-	const result = await User.create(user);
-	return res.status(200).json(result);
-});
-
-const get_users = async_wrapper(async (req, res) => {
-	const results = await User.find().sort("username").distinct("username");
-	results.splice(results.indexOf("Admin"), 1);
-	return res.status(200).json(results);
-});
-
-const send_mail = async (
+const create_send_mail = async (
 	oauth2_client,
 	client_id,
 	client_secret,
@@ -42,18 +28,113 @@ const send_mail = async (
 		});
 
 		const mail_options = {
-			from: `MyHealthCheck <${process.env.AUTHORIZED_EMAIL}>`,
+			from: {
+				name: "AUF MyHealthCheck",
+				address: process.env.AUTHORIZED_EMAIL,
+			},
 			to: target,
-			subject: "Surveyee Assessment Alert",
+			subject: "User Creation",
 			text: `Greetings,
 
-MyHealthCheck Admin has issued a user password change to your account.
+AUF MyHealthCheck Admin has issued a user creation to your account.
 Here is your temporary password: ${password}
 
 Use the given temporary password next time you will login.`,
 			html: `<p style="white-space: pre-line">Greetings,
 
-MyHealthCheck Admin has issued a <b>user password change</b> to your account.
+AUF MyHealthCheck Admin has issued a <b>user creation</b> to your account.
+Here is your temporary password: <b>${password}</b>
+
+Use the given temporary password <b>when you login</b>.`,
+		};
+
+		const results = await transport.sendMail(mail_options);
+
+		return results;
+	} catch (error) {
+		return error;
+	}
+};
+
+const create_user = async_wrapper(async (req, res) => {
+	const client_id = process.env.CLIENT_ID;
+	const client_secret = process.env.CLIENT_SECRET;
+	const redirect_uri = "https://developers.google.com/oauthplayground";
+	const refresh_token = process.env.REFRESH_TOKEN;
+
+	const { username, password } = req.body.user;
+
+	const oauth2_client = new google.auth.OAuth2(
+		client_id,
+		client_secret,
+		redirect_uri
+	);
+
+	oauth2_client.setCredentials({ refresh_token: refresh_token });
+
+	const hashed_password = await bcrypt.hash(password, 12);
+	const user = { username, password: hashed_password };
+
+	await create_send_mail(
+		oauth2_client,
+		client_id,
+		client_secret,
+		refresh_token,
+		username,
+		password
+	);
+
+	const result = await User.create(user);
+	await TemporaryPassword.updateOne({ username }, user, {
+		upsert: true,
+	});
+	return res.status(200).json(result);
+});
+
+const get_users = async_wrapper(async (req, res) => {
+	const results = await User.find().sort("username").distinct("username");
+	results.splice(results.indexOf("Admin"), 1);
+	return res.status(200).json(results);
+});
+
+const update_send_mail = async (
+	oauth2_client,
+	client_id,
+	client_secret,
+	refresh_token,
+	target,
+	password
+) => {
+	try {
+		const access_token = oauth2_client.getAccessToken();
+		const transport = nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				type: "OAuth2",
+				user: process.env.AUTHORIZED_EMAIL,
+				clientId: client_id,
+				clientSecret: client_secret,
+				refreshToken: refresh_token,
+				accessToken: access_token,
+			},
+		});
+
+		const mail_options = {
+			from: {
+				name: "AUF MyHealthCheck",
+				address: process.env.AUTHORIZED_EMAIL,
+			},
+			to: target,
+			subject: "User Password Change",
+			text: `Greetings,
+
+AUF MyHealthCheck Admin has issued a user password change to your account.
+Here is your temporary password: ${password}
+
+Use the given temporary password next time you will login.`,
+			html: `<p style="white-space: pre-line">Greetings,
+
+AUF MyHealthCheck Admin has issued a <b>user password change</b> to your account.
 Here is your temporary password: <b>${password}</b>
 
 Use the given temporary password <b>next time you will login</b>.`,
@@ -86,7 +167,7 @@ const update_user = async_wrapper(async (req, res) => {
 	const hashed_password = await bcrypt.hash(password, 12);
 	const user = { username, password: hashed_password };
 
-	await send_mail(
+	await update_send_mail(
 		oauth2_client,
 		client_id,
 		client_secret,
